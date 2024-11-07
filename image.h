6a940c8eb53a71cd65798280de4514c767a78e2e
Rodion Basmanov <st135699@student.spbu.ru>
2024-11-07 21:47:24 +0300

    add head comment

--- image.h
+++ image.h
@@ -1,10 +1,19 @@
+/* ## LabWork1
+## Author
+Basmanov Rodion B81-mm
+## Contacts
+st135699@student.spbu.ru
+## Description
+LabWork1 */
+
 #ifndef IMAGE_H
 #define IMAGE_H
 
 #include <vector>
 #include "bmp.h"
 
-std::vector<uint8_t> rotate90Clockwise(const BMPHeader &header, const std::vector<uint8_t> &data);
-std::vector<uint8_t> applyGaussianFilter(const BMPHeader &header, const std::vector<uint8_t> &data);
+std::vector<uint8_t> rotate90Clockwise(BMPHeader &header, const std::vector<uint8_t> &data);
+std::vector<uint8_t> rotate90CounterClockwise(BMPHeader &header, const std::vector<uint8_t> &data);
+std::vector<uint8_t> applyGaussianFilter(BMPHeader &header, const std::vector<uint8_t> &data);
 
 #endif
