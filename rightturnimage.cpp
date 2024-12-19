4a869644165baf3c0c09495c4d75d94aa4bef688
Rodion Basmanov <st135699@student.spbu.ru>
2024-12-20 02:50:14 +0300

    added rightturnimage.cpp

---
+++ rightturnimage.cpp
@@ -0,0 +1,31 @@
+/* ## LabWork1
+## Author
+Basmanov Rodion B81-mm
+## Contacts
+st135699@student.spbu.ru
+## Description
+LabWork1 */
+
+#include <iostream>
+#include "image.h"
+#include "turnimage.h"
+#include <vector>
+
+void Turn_Image::RightTurn(const char* path)
+{
+    Image image(0, 0);
+    image.Read(path);
+    int height = image.GetHeight();
+    int width = image.GetWidth();
+    Image rotatedImage(height, width);
+
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            rotatedImage.SetColor(image.GetColor(x, y), height - y - 1, x);
+        }
+    }
+
+    rotatedImage.Export("Right_rotated_image.bmp");
+}
