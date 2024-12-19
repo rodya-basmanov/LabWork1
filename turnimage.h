540fe343d8ae510588093d89effc11d406e4f0ec
Rodion Basmanov <st135699@student.spbu.ru>
2024-12-20 02:50:38 +0300

    added turnimage.h

---
+++ turnimage.h
@@ -0,0 +1,18 @@
+/* ## LabWork1
+## Author
+Basmanov Rodion B81-mm
+## Contacts
+st135699@student.spbu.ru
+## Description
+LabWork1 */
+
+#ifndef TURNIMAGE_H
+#define TURNIMAGE_H
+
+namespace Turn_Image
+{
+    void RightTurn(const char* path);
+    void LeftTurn(const char* path);
+}
+
+#endif
